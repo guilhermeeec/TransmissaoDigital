16948398b99d203405704187185c7fbba1dd2ecf
Guilherme Araujo Thomaz <guilhermepic2016@gmail.com>
2023-10-12 20:04:15 -0300

    Treinamento adversarial

--- model_gradients.m
+++ model_gradients.m
@@ -3,6 +3,16 @@ function [gradients,state,loss] = model_gradients(net,dl_x,dl_y)
     [dl_y_pred,state] = forward(net,dl_x);          % Passa conjunto pela rede
     
     loss = mse(dl_y_pred,dl_y);                     % Calcula perda em relação ao rótulo
+    
+    %loss = single(dlarray(immse(double(extractdata(dl_y_pred)),double(extractdata(dl_y)))));                     
+    %power_dl_y_pred = sqrt(dl_y_pred(1:1024,:).^2 + dl_y_pred(1025:2048,:).^2)
+    %power_dl_y = sqrt(dl_y(1:1024,:).^2 + dl_y(1025:2048,:).^2)
+    
+    % dl_y = dl_y(1:1024,:) + dl_y(1025:2048,:)*1i
+    % dl_y_pred = dl_y_pred(1:1024,:) + dl_y_pred(1025:2048,:)*1i
+    % loss = mse(dl_y_pred,dl_y);                     % Calcula perda em relação ao rótulo
+
+    
     gradients = dlgradient(loss,net.Learnables);
 
     loss = double(gather(extractdata(loss)));
--- model_gradients_input.m
+++ model_gradients_input.m
@@ -5,6 +5,8 @@ function gradient = model_gradients_input(dlnet,dl_x,y)
     [dl_y_pred] = forward(dlnet,dl_x);
 
     loss = mse(dl_y_pred,y);
+    %loss = immse(double(extractdata(dl_y_pred)),double(extractdata(y)));                     
+
     
     gradient = dlgradient(loss,dl_x);
 
