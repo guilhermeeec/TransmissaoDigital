5b0ba93d802aaa18972b5909b3f89cfdd883f929
Guilherme Araujo Thomaz <guilhermepic2016@gmail.com>
2023-10-24 12:19:06 -0300

    Variacao da potencia do ataque aleatorio

--- variable_random_attack.m
+++ variable_random_attack.m
@@ -9,25 +9,20 @@ test_name = '..\databases\test.csv';
 test_name_orig = '..\databases\test_orig.csv';
 random_name = '..\databases\random.csv';
 random_name_orig = '..\databases\random_orig.csv';
-errors_ls = [];
 errors_ls_random = [];
-errors_nn = [];
 errors_nn_random = [];
-for snr = [20 30 40]
-    error_ls = generate_dataset_clean(3000,snr,5,train_name,train_name_orig);
-    errors_ls = [errors_ls error_ls]
-    
-    generate_dataset_clean(1000,snr,5,test_name,test_name_orig);
-    
-    error_ls_random = generate_dataset_attacked(1000,snr,5,0.01,random_name,random_name_orig);
+snr = 30;
+
+generate_dataset_clean(3000,snr,5,train_name,train_name_orig);
+dlnet = train_network_clean(layers,150,train_name);
+
+generate_dataset_clean(1000,snr,5,test_name,test_name_orig);
+error_nn = test_network(dlnet,test_name,test_name_orig)
+
+for power = [0.001 0.01 0.1]  
+    error_ls_random = generate_dataset_attacked(1000,snr,5,power,random_name,random_name_orig);
     errors_ls_random = [errors_ls_random error_ls_random]
-    
-    dlnet = train_network_clean(layers,150,train_name);
-    
-    error_nn = test_network(dlnet,test_name,test_name_orig);
-    errors_nn = [errors_nn error_nn]
     
     error_nn_random = test_network(dlnet,random_name,random_name_orig);
     errors_nn_random = [errors_nn_random error_nn_random]
-   
 end
